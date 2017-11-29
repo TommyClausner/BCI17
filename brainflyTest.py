8c1aa5a0f92378ae6951ec105c3d86b180ef266a
Tomminsky <33838917+Tomminsky@users.noreply.github.com>
2017-11-30 00:48:01 +0100

    Add files via upload

---
+++ brainflyTest.py
@@ -0,0 +1,196 @@
+import matplotlib
+import numpy as np
+matplotlib.rcParams['toolbar']='None'
+from psychopy import visual, core,event
+
+grid_size=100
+window_size=800
+
+trialtime=4.5 # time per trial
+numtrials_per_cond=20
+
+mywin=visual.Window([window_size,window_size*0.75],color=(-1,-1,-1),units='pix',monitor='testMonitor',winType="pygame")
+
+class stimuli_(object):
+        def __init__(self,mywin):
+            self.mywin=mywin
+            win_s = self.mywin.size
+
+            self.pattern2 = visual.Circle(win=self.mywin, pos=[win_s[0] / 2 - win_s[0] / 10 + 10, -win_s[1] / 2 + win_s[1] / 10],
+                                     radius=win_s[0] / 16, edges=32, fillColor=[1, -1, -1], lineColor=[-1, -1, -1],
+                                     units='pix')
+            self.pattern3 = visual.Circle(win=self.mywin, pos=[-win_s[0] / 2 + win_s[0] / 10 - 10, -win_s[1] / 2 + win_s[1] / 10],
+                                     radius=win_s[0] / 16, edges=32, fillColor=[-1, -1, 1], lineColor=[-1, -1, -1],
+                                     units='pix')
+            self.pattern4 = visual.ShapeStim(win=self.mywin, vertices=(
+            [-win_s[0] / 2, -(win_s[1] / 2 - win_s[1] / 5 - 10)], [-win_s[0] / 2, -(win_s[1] / 2 - win_s[1] / 5 - 9)],
+            [win_s[0] / 2, -(win_s[1] / 2 - win_s[1] / 5 - 9)], [win_s[0] / 2, -(win_s[1] / 2 - win_s[1] / 5 - 10)]),
+                                        lineColor=[1, 1, 1], units='pix')
+
+            self.Trialclock = core.Clock()
+
+            self.start_time1 = self.Trialclock.getTime()
+            self.start_time2 = self.Trialclock.getTime()
+            self.start_time3 = self.Trialclock.getTime()
+            self.start_time4 = self.Trialclock.getTime()
+
+            self.pattern2.setAutoDraw(True)
+            self.pattern3.setAutoDraw(True)
+            self.pattern4.setAutoDraw(True)
+
+
+        def __call__(self,mywin,freq = 15,freq2 = 10):
+            self.mywin=mywin
+            dur = 1. / freq
+            dur2 = 1. / freq2
+
+            if ((self.Trialclock.getTime() - self.start_time1) > (dur * 0.45)):
+                if (dur * 0.45 - (self.Trialclock.getTime() - self.start_time1)) > 0:
+                    core.wait(dur * 0.49 - (self.Trialclock.getTime() - self.start_time1))
+                self.pattern3.setAutoDraw(True)
+
+            if ((self.Trialclock.getTime() - self.start_time2) > (dur2 * 0.45)):
+                if (dur2 * 0.45 - (self.Trialclock.getTime() - self.start_time2)) > 0:
+                    core.wait(dur2 * 0.49 - (self.Trialclock.getTime() - self.start_time2))
+                self.pattern2.setAutoDraw(True)
+
+            if ((self.Trialclock.getTime() - self.start_time1) > (dur * 0.95)):
+
+                if (dur * 0.95 - (self.Trialclock.getTime() - self.start_time1)) > 0:
+                    print(self.Trialclock.getTime() - self.start_time1)
+                    core.wait(dur * 0.99 - (self.Trialclock.getTime() - self.start_time1))
+                self.start_time1 = self.Trialclock.getTime()
+                self.pattern3.setAutoDraw(False)
+
+
+            if ((self.Trialclock.getTime() - self.start_time2) > (dur2 * 0.95)):
+                if (dur2 * 0.95 - (self.Trialclock.getTime() - self.start_time2)) > 0:
+                    core.wait(dur2 * 0.99 - (self.Trialclock.getTime() - self.start_time2))
+                self.start_time2 = self.Trialclock.getTime()
+                self.pattern2.setAutoDraw(False)
+            return self.mywin
+
+class brainfly(object):
+    class enemy(object):
+        def __init__(self,mywin):
+            self.mywin=mywin
+
+
+            self.enemy_one_line=visual.Line(win=self.mywin, start=(-400, 250), end=(400, 250),lineColor='red')
+            self.enemy_one_line.setAutoDraw(True)
+            self.enemy_one = visual.Circle(win=self.mywin,
+                                           pos=[np.random.randint(600) - 300, 250],
+                                           radius=40, edges=32, fillColor=[-1, 1, -1],
+                                           units='pix')
+            self.enemy_one.setAutoDraw(True)
+        def __call__(self,mywin,stepsize=5,radincrease=1):
+            self.mywin = mywin
+            self.enemy_one.setPos([self.enemy_one.pos[0],self.enemy_one.pos[1]-stepsize])
+            self.enemy_one_line.setPos([self.enemy_one_line.pos[0], self.enemy_one_line.pos[1] - stepsize])
+
+            if self.enemy_one.pos[1]<-150:
+                collision_=1
+                self.enemy_one.setAutoDraw(False)
+                self.enemy_one_line.setAutoDraw(False)
+            else:
+                collision_=0
+                self.enemy_one.radius=self.enemy_one.radius+radincrease
+                self.enemy_one.setAutoDraw(True)
+                self.enemy_one_line.setAutoDraw(True)
+
+            return self.mywin,collision_
+
+    class spaceship(object):
+
+        def __init__(self, mywin):
+            self.mywin=mywin
+            self.ship = visual.Rect(win=self.mywin,pos=[0, -150],
+                                           width=40, height=32, fillColor=[1, 1, 1],
+                                           units='pix')
+            self.ship.setAutoDraw(True)
+
+        def __call__(self, mywin,stepsize=10):
+            button=event.getKeys()
+            if len(button)>0:
+                if button[-1]=='left':
+                    self.ship.setPos([self.ship.pos[0] - stepsize, self.ship.pos[1]])
+                if button[-1]=='right':
+                    self.ship.setPos([self.ship.pos[0] + stepsize, self.ship.pos[1]])
+
+                if self.ship.pos[0]<-350:
+                    self.ship.setPos([-350, self.ship.pos[1]])
+
+                if self.ship.pos[0]>350:
+                    self.ship.setPos([350, self.ship.pos[1]])
+
+            return self.mywin
+
+    class cannonball(object):
+
+        def __init__(self, mywin,shippos):
+            self.mywin=mywin
+            self.ball = visual.Circle(win=self.mywin, pos=shippos,
+                                     radius=15, edges=32, fillColor=[1, 1, 1], lineColor=[-1, -1, -1],
+                                     units='pix')
+            self.ball.setAutoDraw(False)
+
+        def __call__(self, mywin,shippos,stepsize=10):
+            self.ball.setPos([self.ball.pos[0],self.ball.pos[1]+stepsize])
+            if self.ball.pos[1]>350:
+                self.ball.setPos(shippos)
+            return self.mywin
+
+    def __init__(self, mywin):
+        self.mywin = mywin
+        win_s = self.mywin.size
+        self.enemy1=brainfly.enemy(self.mywin)
+        self.enemy2 = brainfly.enemy(self.mywin)
+        self.enemy2.enemy_one.setAutoDraw(False)
+        self.enemy2.enemy_one_line.setAutoDraw(False)
+        self.enemyclock = core.Clock()
+        self.start_ememy1=self.enemyclock.getTime()
+
+        self.ship=brainfly.spaceship(self.mywin)
+        self.cannons=list()
+        for n in range(11):
+            self.cannons.append(brainfly.cannonball(self.mywin,self.ship.ship.pos))
+
+        self.initval=1
+
+    def __call__(self, mywin):
+        self.mywin = mywin
+        self.mywin,collision_=self.enemy1(self.mywin)
+        if collision_:
+            self.enemy1 = brainfly.enemy(self.mywin)
+
+        if self.enemy1.enemy_one.pos[1]<50:
+            self.initval=0
+            self.enemy2.enemy_one_line.setAutoDraw(True)
+            self.enemy2.enemy_one.setAutoDraw(True)
+
+        if not self.initval:
+            self.mywin, collision_ = self.enemy2(self.mywin)
+
+        if collision_ and self.enemy1.enemy_one.pos[1]<50:
+            self.enemy2 = brainfly.enemy(self.mywin)
+
+        for n in range(len(self.cannons)-1):
+            if n==0 or abs(self.cannons[n].ball.pos[1]-self.cannons[n+1].ball.pos[1])>50:
+                self.mywin = self.cannons[n+1](self.mywin,self.ship.ship.pos)
+                self.cannons[n+1].ball.setAutoDraw(True)
+
+        self.mywin=self.ship(self.mywin)
+
+        return self.mywin
+
+frametime=1/60.
+
+numtrials_per_cond_act=np.atleast_2d(np.zeros(2))
+
+stim=stimuli_(mywin)
+game=brainfly(mywin)
+while True:
+    mywin=stim(mywin)
+    mywin.flip()
+    mywin = game(mywin)
+    mywin.flip()
