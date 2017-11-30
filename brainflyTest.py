d5bdfbaa2cbd425a7beb432c7a5ad63f15fc1568
Tomminsky <tommy.clausner@gmail.com>
2017-12-01 00:05:09 +0100

    backup version for unix
    Warning: explosive ;)

--- brainflyTest.py
+++ brainflyTest.py
@@ -3,69 +3,63 @@
 bufferpath = "../../dataAcq/buffer/python"
 sigProcPath = "../signalProc"
 
+braincontrol=0
+
+
 import sys,os
 from time import sleep
 import matplotlib
 import numpy as np
 matplotlib.rcParams['toolbar']='None'
-from psychopy import visual, core,event
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),bufferpath))
-import FieldTrip
-import bufhelp
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),sigProcPath))
+from psychopy import visual, core
+import pygame
+from PIL import Image
 
-## CONFIGURABLE VARIABLES
-# Connection options of fieldtrip, hostname and port of the computer running the fieldtrip buffer.
-hostname='localhost'
-port=1972
+if braincontrol:
+    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),bufferpath))
+    import FieldTrip
+    import bufhelp
+    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),sigProcPath))
 
-## init connection to the buffer
-timeout=5000
-(ftc,hdr) = bufhelp.connect(hostname,port)
+    ## CONFIGURABLE VARIABLES
+    # Connection options of fieldtrip, hostname and port of the computer running the fieldtrip buffer.
+    hostname='localhost'
+    port=1972
 
-# Wait until the buffer connects correctly and returns a valid header
-hdr = None
-while hdr is None :
-    print(('Trying to connect to buffer on %s:%i ...'%(hostname,port)))
-    try:
-        ftc.connect(hostname, port)
-        print('\nConnected - trying to read header...')
-        hdr = ftc.getHeader()
-    except IOError:
-        pass
+    ## init connection to the buffer
+    timeout=5000
+    (ftc,hdr) = bufhelp.connect(hostname,port)
 
-    if hdr is None:
-        print('Invalid Header... waiting')
-        sleep(1)
-    else:
-        print(hdr)
-        print((hdr.labels))
-fSample = hdr.fSample
+    # Wait until the buffer connects correctly and returns a valid header
+    hdr = None
+    while hdr is None :
+        print(('Trying to connect to buffer on %s:%i ...'%(hostname,port)))
+        try:
+            ftc.connect(hostname, port)
+            print('\nConnected - trying to read header...')
+            hdr = ftc.getHeader()
+        except IOError:
+            pass
 
-def sendEvent(event_type, event_value=1, sample=-1):
-    e = FieldTrip.Event()
-    e.type=event_type
-    e.value=event_value
-    e.sample=sample
-    ftc.putEvents(e)
+        if hdr is None:
+            print('Invalid Header... waiting')
+            sleep(1)
+        else:
+            print(hdr)
+            print((hdr.labels))
+    fSample = hdr.fSample
 
-def processBufferEvents():
-    global running
-    events = ftc.getEvents()
-    events=events[-1]
-    if events.type == 'classifier.prediction':
-        pred = events.value
-        game.ship.direction=pred+0
-        sendEvent('stim.target', 1)
-    else:
-        game.ship.direction = 0
+    def sendEvent(event_type, event_value=1, sample=-1):
+        e = FieldTrip.Event()
+        e.type=event_type
+        e.value=event_value
+        e.sample=sample
+        ftc.putEvents(e)
 
-
-grid_size=100
 window_size=800
 
 numtrials_per_cond=20
-
+os.environ['SDL_VIDEODRIVER'] = 'Quartz'
 mywin=visual.Window([window_size,window_size*0.75],color=(-1,-1,-1),units='pix',monitor='testMonitor',winType="pygame")
 
 class stimuli_(object):
@@ -96,8 +90,7 @@ class stimuli_(object):
             self.pattern4.setAutoDraw(True)
 
 
-        def __call__(self,mywin,freq = 15,freq2 = 10):
-            self.mywin=mywin
+        def __call__(self,freq = 15,freq2 = 10):
             dur = 1. / freq
             dur2 = 1. / freq2
 
@@ -125,131 +118,213 @@ class stimuli_(object):
                     core.wait(dur2 * 0.99 - (self.Trialclock.getTime() - self.start_time2))
                 self.start_time2 = self.Trialclock.getTime()
                 self.pattern2.setAutoDraw(False)
-            return self.mywin
 
-class brainfly(object):
-    class enemy(object):
-        def __init__(self,mywin):
-            self.mywin=mywin
+class enemy(object):
+    def __init__(self,mywin):
+        self.mywin=mywin
 
 
-            self.enemy_one_line=visual.Line(win=self.mywin, start=(-400, 250), end=(400, 250),lineColor='red')
-            self.enemy_one_line.setAutoDraw(True)
-            self.enemy_one = visual.Circle(win=self.mywin,
-                                           pos=[np.random.randint(600) - 300, 250],
-                                           radius=40, edges=32, fillColor=[-1, 1, -1],
-                                           units='pix')
-            self.enemy_one.setAutoDraw(True)
-        def __call__(self,mywin,stepsize=5,radincrease=1):
-            self.mywin = mywin
-            self.enemy_one.setPos([self.enemy_one.pos[0],self.enemy_one.pos[1]-stepsize])
-            self.enemy_one_line.setPos([self.enemy_one_line.pos[0], self.enemy_one_line.pos[1] - stepsize])
+        self.enemy_line=visual.Line(win=self.mywin, start=(-400, 250), end=(400, 250),lineColor='red')
+        self.enemy_line.setAutoDraw(True)
+        self.enemy = visual.Circle(win=self.mywin,
+                                       pos=[np.random.randint(600) - 300, 250],
+                                       radius=40, edges=32, fillColor=[-1, 1, -1],
+                                       units='pix')
+        self.enemy.setAutoDraw(True)
 
-            if self.enemy_one.pos[1]<-150:
-                collision_=1
-                self.enemy_one.setAutoDraw(False)
-                self.enemy_one_line.setAutoDraw(False)
+    def __call__(self,stepsize=2,radincrease=0.4):
+        self.enemy.setPos([self.enemy.pos[0],self.enemy.pos[1]-stepsize])
+        self.enemy_line.setPos([self.enemy_line.pos[0], self.enemy_line.pos[1] - stepsize])
+        self.enemy.radius = self.enemy.radius + radincrease
+        pass
+
+class spaceship(object):
+
+    def __init__(self, mywin,braincontrol=0):
+        self.mywin=mywin
+        self.braincontrol=braincontrol
+        self.ship = visual.Rect(win=self.mywin,pos=[0, -150],
+                                       width=40, height=32, fillColor=[1, 1, 1],
+                                       units='pix')
+        self.ship.setAutoDraw(True)
+        self.direction=0
+
+    def __call__(self,stepsize=10):
+
+        if self.braincontrol:
+            stepsize=50
+            events = ftc.getEvents()
+            events = events[-1]
+            if events.type == 'classifier.prediction':
+                pred = events.value
+                self.direction = pred
+                sendEvent('stim.target', 1)
             else:
-                collision_=0
-                self.enemy_one.radius=self.enemy_one.radius+radincrease
-                self.enemy_one.setAutoDraw(True)
-                self.enemy_one_line.setAutoDraw(True)
+                self.direction = 0
+        else:
 
-            return self.mywin,collision_
+            if pygame.key.get_pressed()[pygame.K_LEFT]:
+                self.direction=1
+            elif pygame.key.get_pressed()[pygame.K_RIGHT]:
+                self.direction = -1
+            else:
+                self.direction = 0
+        self.ship.setPos([self.ship.pos[0] -self.direction* stepsize, self.ship.pos[1]])
 
-    class spaceship(object):
+        if self.ship.pos[0]<-350:
+            self.ship.setPos([-350, self.ship.pos[1]])
 
-        def __init__(self, mywin):
-            self.mywin=mywin
-            self.ship = visual.Rect(win=self.mywin,pos=[0, -150],
-                                           width=40, height=32, fillColor=[1, 1, 1],
-                                           units='pix')
-            self.ship.setAutoDraw(True)
-            self.direction=1
+        if self.ship.pos[0]>350:
+            self.ship.setPos([350, self.ship.pos[1]])
 
-        def __call__(self, mywin,stepsize=50):
+        pass
 
-            self.ship.setPos([self.ship.pos[0] -self.direction* stepsize, self.ship.pos[1]])
-
-            if self.ship.pos[0]<-350:
-                self.ship.setPos([-350, self.ship.pos[1]])
-
-            if self.ship.pos[0]>350:
-                self.ship.setPos([350, self.ship.pos[1]])
-
-            return self.mywin
-
-    class cannonball(object):
+class cannonball(object):
 
         def __init__(self, mywin,shippos):
             self.mywin=mywin
             self.ball = visual.Circle(win=self.mywin, pos=shippos,
                                      radius=15, edges=32, fillColor=[1, 1, 1], lineColor=[-1, -1, -1],
                                      units='pix')
-            self.ball.setAutoDraw(False)
+            self.ball.setAutoDraw(True)
 
-        def __call__(self, mywin,shippos,stepsize=10):
+        def __call__(self,stepsize=10):
             self.ball.setPos([self.ball.pos[0],self.ball.pos[1]+stepsize])
-            if self.ball.pos[1]>350:
-                self.ball.setPos(shippos)
-            return self.mywin
+            pass
 
-    def __init__(self, mywin):
+class explosionanim(object):
+    def __init__(self, mywin,explosion,exppos):
         self.mywin = mywin
-        win_s = self.mywin.size
-        self.enemy1=brainfly.enemy(self.mywin)
-        self.enemy2 = brainfly.enemy(self.mywin)
-        self.enemy2.enemy_one.setAutoDraw(False)
-        self.enemy2.enemy_one_line.setAutoDraw(False)
-        self.enemyclock = core.Clock()
-        self.start_ememy1=self.enemyclock.getTime()
+        self.gif=explosion
+        self.expl = visual.ImageStim(self.mywin,pos=exppos)
+        self.expl.setAutoDraw(False)
+        self.frame_counter=0
+        self.frameorder=range(7,17)+range(6)
 
-        self.ship=brainfly.spaceship(self.mywin)
-        self.cannons=list()
-        for n in range(11):
-            self.cannons.append(brainfly.cannonball(self.mywin,self.ship.ship.pos))
+    def __call__(self):
+        self.expl.setAutoDraw(False)
+        self.gif.seek(self.frameorder[self.frame_counter])
+        self.expl.setImage(self.gif)
+        self.expl.setAutoDraw(True)
+        self.frame_counter += 1
+    pass
 
-        self.initval=1
+enemies=[]
+balls=[]
 
-    def __call__(self, mywin):
-        self.mywin = mywin
-        self.mywin,collision_=self.enemy1(self.mywin)
-        if collision_:
-            self.enemy1 = brainfly.enemy(self.mywin)
-
-        if self.enemy1.enemy_one.pos[1]<50:
-            self.initval=0
-            self.enemy2.enemy_one_line.setAutoDraw(True)
-            self.enemy2.enemy_one.setAutoDraw(True)
-
-        if not self.initval:
-            self.mywin, collision_ = self.enemy2(self.mywin)
-
-        if collision_ and self.enemy1.enemy_one.pos[1]<50:
-            self.enemy2 = brainfly.enemy(self.mywin)
-
-        for n in range(len(self.cannons)-1):
-            if n==0 or abs(self.cannons[n].ball.pos[1]-self.cannons[n+1].ball.pos[1])>50:
-                self.mywin = self.cannons[n+1](self.mywin,self.ship.ship.pos)
-                self.cannons[n+1].ball.setAutoDraw(True)
-
-        self.mywin=self.ship(self.mywin)
-
-        return self.mywin
-
-frametime=1/60.
-
-numtrials_per_cond_act=np.atleast_2d(np.zeros(2))
-
+ship=spaceship(mywin,braincontrol)
+enemies.append(enemy(mywin))
 stim=stimuli_(mywin)
-game=brainfly(mywin)
-raw_input("Press any key to continue...")
-sendEvent('stim.target', 1)
 
-while True:
-    mywin=stim(mywin)
-    mywin.flip()
-    mywin = game(mywin)
-    mywin.flip()
+Timerobj=core.Clock()
+enemtimer=Timerobj.getTime()
+canontimer=Timerobj.getTime()
 
-    processBufferEvents()
+deaths=0
+hits=0
+
+gamedur=Timerobj.getTime()
+fpstimer=Timerobj.getTime()
+
+hud_string='Time played: '+str(round(Timerobj.getTime()-gamedur,1))+' | hits: '+str(hits)+' | deaths: '+str(deaths)
+
+HUD_ = visual.TextStim(win=mywin,pos=[-400,300],text=hud_string,color=[1, 1, 1],units='pix',height=20,alignVert='top',alignHoriz='left')
+HUD_.setAutoDraw(True)
+
+FPS_ = visual.TextStim(win=mywin,pos=[400,300],text='0',color=[0, 1, 1],units='pix',height=40,alignVert='top',alignHoriz='right')
+FPS_.setAutoDraw(True)
+
+fps_timer_col=[]
+
+enable_explosions=1
+
+if enable_explosions:
+    explosiongif = Image.open("explosion2.gif")
+    explo = explosionanim(mywin, explosiongif, [0,0])
+    explosions=[]
+    show_explosion = 0
+
+if braincontrol:
+    sendEvent('stim.target', 1)
+
+while Timerobj.getTime() - gamedur<=90:
+
+    # create enemies
+    if Timerobj.getTime()-enemtimer>3:
+        enemtimer = Timerobj.getTime()
+        enemies.append(enemy(mywin))
+
+    # update enemy pos
+    if len(enemies)>0:
+        for enem in enemies:
+            enem()
+            if enem.enemy.pos[1]<ship.ship.pos[1]:
+                enem.enemy.setAutoDraw(False)
+                enem.enemy_line.setAutoDraw(False)
+                enemies.remove(enem)
+                deaths+=1
+
+    # update ship pos
+    ship()
+
+    # create cannonballs
+    if Timerobj.getTime() - canontimer > 0.3:
+        canontimer = Timerobj.getTime()
+        balls.append(cannonball(mywin, ship.ship.pos))
+
+    # update cannonballs pos
+    if len(balls) > 0:
+        for ball in balls:
+
+            if len(enemies) > 0:
+                for enem in enemies:
+
+                    if np.sqrt((ball.ball.pos[0] - enem.enemy.pos[0]) ** 2 + (
+                                ball.ball.pos[1] - enem.enemy.pos[1]) ** 2) < enem.enemy.radius:
+                        ball.ball.setAutoDraw(False)
+                        balls.remove(ball)
+                        if enable_explosions:
+                            show_explosion = 1
+                            explosion_pos=enem.enemy.pos
+                        enem.enemy.setAutoDraw(False)
+                        enem.enemy_line.setAutoDraw(False)
+                        enemies.remove(enem)
+                        hits+=1
+
+                    else:
+                        if ball.ball.pos[1] > enem.enemy.pos[1]:
+                            ball.ball.setAutoDraw(False)
+                            balls.remove(ball)
+                        else:
+                            if ball.ball.pos[1] > 400:
+                                ball.ball.setAutoDraw(False)
+                                balls.remove(ball)
+            ball()
+
+    # update stimuli pos
+    stim()
+
+    # update explosion anim
+    if enable_explosions:
+        if show_explosion:
+            explosions.append(explosionanim(mywin, explosiongif, explosion_pos))
+            show_explosion=0
+
+        for exp_ in explosions:
+            if exp_.frame_counter>15:
+                exp_.expl.setAutoDraw(False)
+                explosions.remove(exp_)
+            else:
+                exp_()
+
+    # update HUD
+    hud_string = 'Time played: ' + str(round(Timerobj.getTime() - gamedur, 1)) + ' | hits: ' + str(
+        hits) + ' | deaths: ' + str(deaths)
+    HUD_.setText(hud_string)
+    fps_timer_col.append(1. / (Timerobj.getTime() - fpstimer))
+    FPS_.setText(str(int(np.round(np.mean(fps_timer_col)))))
+    if len(fps_timer_col)>120:
+        del fps_timer_col[0]
+    fpstimer = Timerobj.getTime()
+
+    mywin.flip()
