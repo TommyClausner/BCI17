c68dde2870c0ca0a6d891ad41b7db73ec663e8e6
Tomminsky <tommy.clausner@gmail.com>
2017-12-06 19:01:14 +0100

    restructured the git
    no worries I made a backup copy of the stuff

--- run_game.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#use: sh run_game.sh <matlabroot path> <path to script to execute> <python interpreter path> <python script path>
-
-#example: sh run_game.sh /Applications/MATLAB_Release.app /Users/User/scripts/MATLABscript.m  /usr/bin/ipython /Users/User/scripts/Pythonscript.py
-
-$1/bin/matlab -nodisplay -nodesktop -r "run('$2')" &
-sleep 10s
-$3 $4
