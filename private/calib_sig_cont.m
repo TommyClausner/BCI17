7892dcfaab2c137e310b25857620670d7f27500e
kstandvoss <kstandvoss@uos.de>
2017-12-10 21:02:34 +0100

    bug fixes continuous calibration

--- private/calib_sig_cont.m
+++ private/calib_sig_cont.m
@@ -26,29 +26,29 @@ cap_=cap_(1:end-4);
 trlen_ms=1000;%samples
 state=hdr;
 
-all_data = [];
-all_devents = false;
 while true
-	[data,devents,state]=buffer_waitData(buffhost,buffport,state,'startSet',{'stim.target'},'trlen_samp',trlen_ms,'exitSet',{'data'});
-	if devents(end).value==0
-	    break
+	[data,devents,state]=buffer_waitData(buffhost,buffport,state,'startSet',{'stim.target'},'trlen_samp',trlen_ms,'exitSet',{'data' 'stim.training' 'end'});
+	if ( matchEvents(devents(end),'stim.training','end') )
+		break
 	end
-	all_data = [all_data data.buf];
-	if(~all_devents)
+	if(~exist('all_data'))
+		all_data = data;
 		all_devents = devents;
 	else
+		all_data = [all_data data];
 		all_devents = [all_devents devents];
 	end
-	%retrain classifier
-	[clsfr,res,X,Y] = buffer_train_ersp_clsfr(all_data,all_devents,hdr,'spatialfilter','CAR','freqband',[0.1 1 30 31],'badtrrm',0,'badchrm',0,'capFile',cap_,'overridechnms',1,'visualize',0);
-	f = res.opt.tstf; %predictions
-	pred = sign(f); %predicted classes
-	sendEvent('classifier.prediction',pred);
-	numY = ones(1,length(Y));
-	numY(find(strcmp(Y,'2 RH'))) = -1;
-	fprintf('Clsfr perf = %g\n',sum(pred==Y)./numel(pred)) %print accuracy
+
+	if(mod(size(all_data,2),10) == 0)
+		%retrain classifier
+		[clsfr,res,X,Y] = buffer_train_ersp_clsfr(all_data,all_devents,hdr,'spatialfilter','CAR','freqband',[0.1 1 30 31],'badtrrm',0,'badchrm',0,'capFile',cap_,'overridechnms',1,'visualize',0);
+		f = res.opt.tstf; %predictions
+		pred = (f>0)*2-1; %predicted classes
+		numY = ones(1,length(Y));
+		numY(find(strcmp(Y,'1 LH'))) = -1;
+		fprintf('Clsfr perf = %g\n',sum(pred==numY')./numel(pred)) %print accuracy
+	end
 end
 
 num = numel(dir([CLASSIFIER_PATH filesep '*.mat'])) + 1;
 save([CLASSIFIER_PATH filesep 'clsfr' num2str(num)],'-struct','clsfr');
-exit
